2320d74cfffa4df3fabddb6e3c45f36369a83ae4
abhay8696 <abhaykamble1346@gmail.com>
2023-10-31 15:03:30 +0530

    added basePath with ternary operator in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProduction = process.env.NODE_ENV
 const nextConfig = {
-    output: "export"
+    output: "export",
+    basePath: isProduction === 'production' ? '/logistics-company' : '',
 }
 
 module.exports = nextConfig
