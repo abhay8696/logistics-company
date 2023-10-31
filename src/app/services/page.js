d54c8e43bd9dcbf7cfbb70c0f228c0253cf052eb
abhay8696 <abhaykamble1346@gmail.com>
2023-10-31 19:48:45 +0530

    TEST-changed img src with prefix in services page for production env

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -18,16 +18,20 @@ const prompt = Prompt({
 
 const Services = () => {
 
+    let prefix = '/logistics-company';
+    const environment = process.env.NODE_ENV;
+
+    if(environment === 'development') prefix = '';
 
     const displayServicesBox = ()=> {
 
         const servicesObj = [
-            {name: 'FREIGHT FORWARDING', icon: '/freightForwarding.svg', desc: 'We offer seamless global freight forwarding solutions'},
-            {name: 'CUSTOM CLEARANCE', icon: '/custom-clearance.svg', desc: 'Simplify global trade with hassle-free import/export compliance through our services'},
-            {name: 'TRANSPORTATION', icon: '/transportation.svg', desc: 'Efficient global transportation solutions for your cargo, from start to finish'},
-            {name: 'TRUCKING', icon: "/trucking.svg", desc: "Trucking solutions for reliable cargo transport, across the nation and beyond"},
-            {name: 'WAREHOUSING', icon: '/warehousing.svg', desc: 'Secure, flexible warehousing solutions for your storage and distribution needs'},
-            {name: 'BROKING', icon: '/broking.svg', desc: 'Expert logistics brokering for cost-effective and efficient supply chain solutions'},
+            {name: 'FREIGHT FORWARDING', icon: `${prefix}/freightForwarding.svg`, desc: 'We offer seamless global freight forwarding solutions'},
+            {name: 'CUSTOM CLEARANCE', icon: `${prefix}/custom-clearance.svg`, desc: 'Simplify global trade with hassle-free import/export compliance through our services'},
+            {name: 'TRANSPORTATION', icon: `${prefix}/transportation.svg`, desc: 'Efficient global transportation solutions for your cargo, from start to finish'},
+            {name: 'TRUCKING', icon: `${prefix}/trucking.svg`, desc: "Trucking solutions for reliable cargo transport, across the nation and beyond"},
+            {name: 'WAREHOUSING', icon: `${prefix}/warehousing.svg`, desc: 'Secure, flexible warehousing solutions for your storage and distribution needs'},
+            {name: 'BROKING', icon: `${prefix}/broking.svg`, desc: 'Expert logistics brokering for cost-effective and efficient supply chain solutions'},
         ]
 
         let arr = []
@@ -50,9 +54,9 @@ const Services = () => {
 
     const displyAdditional = ()=> {
         const additionalServices = [
-            {name: 'GENERATE E-WAY BILLS FOR IMPORT SHIPMENTS', icon: '/bill.svg', desc: 'Creating e-way bills for hassle-free import shipment documentation and tracking'},
-            {name: 'GENERATE SIMS & NFMIMS FOR IMPORT SHIPMENTS', icon: '/computer.svg', desc: 'Streamlining import compliance with SIMS and NFMIMS documentation for smooth logistics'},
-            {name: 'DPD REGISTRATION', icon: '/register.svg', desc: 'Efficient DPD registration for temporary import and re-export logistics solutions'},
+            {name: 'GENERATE E-WAY BILLS FOR IMPORT SHIPMENTS', icon: `${prefix}/bill.svg`, desc: 'Creating e-way bills for hassle-free import shipment documentation and tracking'},
+            {name: 'GENERATE SIMS & NFMIMS FOR IMPORT SHIPMENTS', icon: `${prefix}/computer.svg`, desc: 'Streamlining import compliance with SIMS and NFMIMS documentation for smooth logistics'},
+            {name: 'DPD REGISTRATION', icon: `${prefix}/register.svg`, desc: 'Efficient DPD registration for temporary import and re-export logistics solutions'},
         ]
 
         let arr = [];
